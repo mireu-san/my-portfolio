7989994b9e04635a6cfdaf0763d5bb4616993ffe
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-04-08 16:35:42 +0900

    Added logo. Moment description: going to fix the menu bar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,37 @@
-import React from 'react'
+import React from 'react';
+import logoMireu from "../components/logo.png";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Mireu</a>
+
+            <a className="navbar-brand mr-auto" href="#"><img src={logoMireu} alt="logo mireu chronicle"/></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">About me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Portfolio</a>
+                    </li>
+                    
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contacts</a>
+                    </li>
 
-            </ul>
+                </ul>
+                </div>
             </div>
-        </div>
-        </nav>
-            )
-        }
+            </nav>
+                )
+            }
 
 export default Navbar;
